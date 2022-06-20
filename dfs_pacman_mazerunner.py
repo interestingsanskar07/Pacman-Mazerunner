62687346d6f0bf51c8f337c14e3521d495170a2d
interestingsanskar07 <79186265+interestingsanskar07@users.noreply.github.com>
2022-06-20 17:16:01 +0530

    Add files via upload

---
+++ dfs_pacman_mazerunner.py
@@ -0,0 +1,102 @@
+from PIL import Image, ImageDraw
+images = []
+
+a = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+zoom = 15
+borders = 3
+i_start,j_start = 1,1
+i_end,j_end = 13,19
+
+def draw_matrix(a, the_path=[]):
+    im = Image.new('RGB', (zoom * len(a[0]), zoom * len(a)), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            color = (255, 255, 255)
+            r = 0
+            if a[i][j] == 1:
+                color = (0, 0, 0)
+            if i == i_start and j == j_start:
+                color = (0, 255, 0)
+                r = borders
+            if i == i_end and j == j_end:
+                color = (0, 255, 0)
+                r = borders
+            draw.rectangle((j*zoom+r, i*zoom+r, j*zoom+zoom-r-1, i*zoom+zoom-r-1), fill=color)
+            if a[i][j] == 2:
+                r = borders
+                draw.ellipse((j * zoom + r, i * zoom + r, j * zoom + zoom - r - 1, i * zoom + zoom - r - 1),
+                               fill=(255, 255, 0))
+    for u in range(len(the_path)-1):
+        y = the_path[u][0]*zoom + int(zoom/2)
+        x = the_path[u][1]*zoom + int(zoom/2)
+        y1 = the_path[u+1][0]*zoom + int(zoom/2)
+        x1 = the_path[u+1][1]*zoom + int(zoom/2)
+        draw.line((x,y,x1,y1), fill=(255, 0, 0), width=5)
+    draw.rectangle((0, 0, zoom * len(a[0]), zoom * len(a)), outline=(0, 255, 0), width=2)
+    images.append(im)
+
+
+path = []
+m = []
+k = 0
+
+def dfs(i,j):
+    global path, i_end,j_end, a, k
+    if i<0 or j<0 or i>len(a)-1 or j>len(a[0])-1:
+        return
+    if (i,j) in path or a[i][j]>0:
+        return
+
+    k += 1
+
+    path.append((i,j))
+    a[i][j] = 2
+    draw_matrix(a,path)
+    if(i,j) == (i_end,j_end):
+        for animate in range(10):
+            if animate % 2 == 0:
+                draw_matrix(a, path)
+            else:
+                draw_matrix(a)
+        m = path
+        print(m)
+        path.pop()
+        return
+    else:
+        dfs(i-1,j)
+        dfs(i+1,j)
+        dfs(i,j-1)
+        dfs(i,j+1)
+    path.pop()
+    draw_matrix(a,path)
+    return
+
+
+
+
+
+dfs(i_start,j_start)
+
+images[0].save('maze.gif',
+               save_all=True, append_images=images[1:],
+               optimize=False, duration=50, loop=0)
